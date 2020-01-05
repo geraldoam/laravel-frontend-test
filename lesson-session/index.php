81732ff405a36e905b0cd8b3c40464be00ace931
Geraldo <geraldoahnert@gmail.com>
2020-01-04 22:19:24 -0300

    Retirando Google API - falhas iniciais

--- lesson-session/index.php
+++ lesson-session/index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html>
-<head lang="pt-br">
+<head>
 
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -41,9 +41,7 @@
 		  	<button type="submit" class="btn btn-sm btn-info">Entrar</button>
 		  	<a class="btn btn-sm btn-secondary" href="registrar.html">Registrar</a>
 		  </div>
-		</form>
-
-		<button type="submit" class="btn btn-sm btn-warning">Google</button>
+		</form>	
 
 	</div>	
 
@@ -53,7 +51,7 @@
 	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 	<script src="https://kit.fontawesome.com/7afebb249a.js" crossorigin="anonymous"></script>
-	<script src="https://apis.google.com/js/platform.js" async defer></script>
+
 
 </body>
 </html>
